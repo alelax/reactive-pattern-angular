690526ba8ad16564246a840f2b0abc5f034905d9
Alessandro <alessandro.lausdei@gmail.com>
2023-11-29 17:17:32 +0100

    inite restyle of home-component in reactive way, add courses service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Course, sortCoursesBySeqNo} from '../model/course';
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
+import { CoursesService } from "../services/courses.service";
+
 
 
 @Component({
@@ -14,18 +14,31 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 })
 export class HomeComponent implements OnInit {
 
+  /********************************/
+  /********************************/
+  /* OLD IMPERATIVE STYLE - START */
+  /********************************/
+  /********************************/
+
+  /*
   beginnerCourses: Course[];
 
   advancedCourses: Course[];
 
-
   constructor(private http: HttpClient, private dialog: MatDialog) {
 
   }
+  */
+
+  /******************************/
+  /******************************/
+  /* OLD IMPERATIVE STYLE - END */
+  /******************************/
+  /******************************/
 
   ngOnInit() {
 
-    this.http.get('/api/courses')
+    /*this.http.get('/api/courses')
       .subscribe(
         res => {
 
@@ -35,13 +48,13 @@ export class HomeComponent implements OnInit {
 
           this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
 
-        });
+        });*/
 
   }
 
   editCourse(course: Course) {
 
-    const dialogConfig = new MatDialogConfig();
+    const dialogConfig: MatDialogConfig<any> = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
@@ -52,6 +65,17 @@ export class HomeComponent implements OnInit {
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
 
   }
+
+
+  /**************************/
+  /**************************/
+  /* REACTIVE STYLE - START */
+  /**************************/
+  /**************************/
+  constructor(
+    private coursesService: CoursesService,
+    private dialog: MatDialog
+  ) {}
 
 }
 
