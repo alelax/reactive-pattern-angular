72367e8e0bb0ca25493a5571ee98bce9acc2c78e
Alessandro <alessandro.lausdei@gmail.com>
2023-12-03 11:38:27 +0100

    local error handling with sahred reactive service

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -5,12 +5,18 @@ import { CoursesService } from "../services/courses.service";
 import { LoadingService } from "../loading/loading.service";
 import { Course } from "../model/course";
 import * as moment from 'moment';
+import { MessagesService } from "../messages/messages.service";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css'],
-  providers: [LoadingService]
+  providers: [
+    LoadingService,
+    MessagesService
+  ]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -23,6 +29,7 @@ export class CourseDialogComponent implements AfterViewInit {
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     private coursesService: CoursesService,
     private loadingService: LoadingService,
+    private messagesService: MessagesService,
     @Inject(MAT_DIALOG_DATA) course:Course
   ) {
 
@@ -42,7 +49,14 @@ export class CourseDialogComponent implements AfterViewInit {
   save() {
     const changes = this.form.value;
 
-    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
+    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes).pipe(
+      catchError( err => {
+        const message = "could not save resource"
+        this.messagesService.showErrors(message);
+        console.log('errors: ', message, err);
+        return throwError(err);
+      })
+    )
 
     this.loadingService.showLoaderUntilCompleted(saveCourse$)
       .subscribe( value => this.dialogRef.close(value))
