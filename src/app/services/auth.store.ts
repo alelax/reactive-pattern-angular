48b4ab0de2177a716f0d19fea5f1056afb1a87f1
Alessandro <alessandro.lausdei@gmail.com>
2024-01-05 19:35:53 +0100

    completed sample login flow

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,8 @@ import { User } from "../model/user";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+const AUTH_DATA = 'auth_data';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,19 +21,28 @@ export class AuthStore {
   constructor(private http: HttpClient) {
     this.isLoggedIn$ = this.user$.pipe(map(user => !!user));
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map(isLoggedIn => !isLoggedIn));
+
+    const user = localStorage.getItem(AUTH_DATA);
+
+    if (user) this.userSubject.next(JSON.parse(user));
+
   }
 
 
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>('/api/login', {email, password})
       .pipe(
-        tap(user => this.userSubject.next(user)),
+        tap(user => {
+          this.userSubject.next(user);
+          localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+        }),
         shareReplay()
       )
   }
 
   logout() {
     this.userSubject.next(null);
+    localStorage.removeItem(AUTH_DATA);
   }
 
 }
